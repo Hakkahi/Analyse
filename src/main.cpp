7911b3c8ee7ac75e382a6d199490065063c9c2d3
Hakkahi <antoinegeorges00@gmail.com>
2017-04-27 10:04:26 +0200

    Detection des mouvements

--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,13 @@ int main(int argc, char ** argv){
   backgroundLearning(cap, mog, 300);
   std::vector<std::tuple<Scalar, Scalar, Scalar>> colorsToDetect;
   //red1
-  colorsToDetect.push_back(std::make_tuple(Scalar(165, 10, 60), Scalar(179, 255, 255), Scalar(0, 0, 255)));
+  /*colorsToDetect.push_back(std::make_tuple(Scalar(165, 10, 60), Scalar(179, 255, 255), Scalar(0, 0, 255)));
   //red2
   colorsToDetect.push_back(std::make_tuple(Scalar(0, 70, 50), Scalar(3, 230, 230), Scalar(0, 0, 255)));
   //blue
   colorsToDetect.push_back(std::make_tuple(Scalar(90, 90, 50), Scalar(130, 255, 255), Scalar(255, 0, 0)));
   //green
-  colorsToDetect.push_back(std::make_tuple(Scalar(42, 20, 20), Scalar(89, 255, 240), Scalar(0, 255, 0)));
+  colorsToDetect.push_back(std::make_tuple(Scalar(42, 20, 20), Scalar(89, 255, 240), Scalar(0, 255, 0)));*/
   //yellow
   colorsToDetect.push_back(std::make_tuple(Scalar(15, 140, 70), Scalar(40, 255, 255), Scalar(0, 255, 255)));
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,7 @@
 #include "utils.hpp"
 
-
+int iLastX = -1;
+int iLastY = -1;
 
 
 /**
@@ -34,18 +35,18 @@ void backgroundLearning(VideoCapture& cap, Ptr<BackgroundSubtractor>& mog, int n
 
 /**
  * Extract the moving element of the @frame (colored picture)
- * and generate a Threshold (in @mogThresold) with only the moving element
+ * and generate a Threshold (in @mogThreshold) with only the moving element
  * @mog trained statistical backgroundSubtractor
  */
 void backgroundSubtractor(Mat& frame, Mat& mogThreshold, Ptr<BackgroundSubtractor>& mog){
 
-  mog->apply(frame, mogThresold, 0);
+  mog->apply(frame, mogThreshold, 0);
 
-  erode(mogThresold, mogThresold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-  dilate(mogThresold, mogThresold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+  erode(mogThreshold, mogThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+  dilate(mogThreshold, mogThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-  dilate(mogThresold, mogThresold, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
-  erode(mogThresold, mogThresold, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
+  dilate(mogThreshold, mogThreshold, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
+  erode(mogThreshold, mogThreshold, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)));
 }
 
 Mat detectColor(Mat& frame, Scalar lowerHSV, Scalar upperHSV, Scalar color){
@@ -56,13 +57,12 @@ Mat detectColor(Mat& frame, Scalar lowerHSV, Scalar upperHSV, Scalar color){
   std::vector<std::vector<Point> > contours;
   std::vector<Vec4i> hierarchy;
 
+
   // Conversion de l'image en RGB en image en BVH
   cvtColor(frame, imgHSV, COLOR_BGR2HSV);
 
   // Création du threshold de l'image en fonction du seuil donné en paramètre
   inRange(imgHSV, lowerHSV, upperHSV, threshold);
-
-  //imshow("HSV", imgHSV);
 
   // Ouverture pour éliminer les petite imperfections de l'image binaire
   erode(threshold, threshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
@@ -70,6 +70,34 @@ Mat detectColor(Mat& frame, Scalar lowerHSV, Scalar upperHSV, Scalar color){
   // Fermeture Pour remplir les petits trous dans l'image binaire
   dilate( threshold, threshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
   erode(threshold, threshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+  Moments oMoments = moments(threshold);
+  double dM01 = oMoments.m01;
+  double dM10 = oMoments.m10;
+  double dArea = oMoments.m00;
+
+  // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero
+  if (dArea > 10000)
+  {
+   //calculate the position of the ball
+   int posX = dM10 / dArea;
+   int posY = dM01 / dArea;
+   //std::cout << "IN: "<< std::endl;
+   if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+   {
+    //Draw a red line from the previous point to the current point
+    //line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+    //circle(imgArea, Point(posX, posY), 10, Scalar(0,0,255), -1);
+      double res = norm(Point(iLastX, iLastY) - Point(posX, posY));
+      //std::cout << "DISTANCE : " << res << std::endl;
+      if(res >= 50){
+        std::cout << "MOUVEMENT MOTHERFUCKER !!!!!!!" << std::endl;
+      }
+   }
+
+   iLastX = posX;
+   iLastY = posY;
+  }
 
   findContours(threshold, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
   for( int i = 0; i< contours.size(); i++ )
